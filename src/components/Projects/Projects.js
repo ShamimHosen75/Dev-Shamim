bd60e137669130f86070c7532fe56110f13c0921
ShamimHosen75 <developer.shamimhosen@gmail.com>
2024-01-06 17:28:42 +0600

    Projects title & desc added

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,10 +1,69 @@
+import { Container } from '@mui/material';
 import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    position: relative;
+    z-index: 1;
+    align-items: center;
+    padding: 40px 0px 80px 0px;
+    @media (max-width: 960px) {
+        padding: 0px;
+    }
+`;
+
+
+const Wrapper = styled.div`
+    position: relative;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    flex-direction: column;
+    width: 100%;
+    max-width: 1350px;
+    padding: 80px 0;
+    gap: 12px;
+    @media (max-width: 960px) {
+        flex-direction: column;
+    }
+`;
+
+const Title = styled.div`
+font-size: 42px;
+text-align: center;
+font-weight: 600;
+margin-top: 20px;
+  color: ${({ theme }) => theme.text_primary};
+  @media (max-width: 768px) {
+      margin-top: 12px;
+      font-size: 32px;
+  }
+`;
+
+const Desc = styled.div`
+    font-size: 18px;
+    text-align: center;
+    max-width: 600px;
+    color: ${({ theme }) => theme.text_secondary};
+    @media (max-width: 768px) {
+        margin-top: 12px;
+        font-size: 16px;
+    }
+`;
 
 const Projects = () => {
   return (
-    <div>
-      <h2>Projects</h2>
-    </div>
+    <Container id="projects">
+      <Wrapper>
+      <Title>Projects</Title>
+        <Desc>
+          I have worked on a wide range of projects. From CMS to Web Apps. Here are some of my projects.
+        </Desc>
+      </Wrapper>
+    </Container>
   );
 };
 

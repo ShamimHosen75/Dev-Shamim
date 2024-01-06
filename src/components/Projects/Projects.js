31e898c29e1eb2a5c01502ad9aefc46dd4b5e1c5
ShamimHosen75 <developer.shamimhosen@gmail.com>
2024-01-06 18:05:15 +0600

    Project tabs added

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,58 +1,7 @@
 import { Container, ToggleButton } from '@mui/material';
 import React from 'react';
-import styled from 'styled-components';
+import { Desc, Title, ToggleButtonGroup, Wrapper } from './ProjectStyles';
 
-const Container = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    position: relative;
-    z-index: 1;
-    align-items: center;
-    padding: 40px 0px 80px 0px;
-    @media (max-width: 960px) {
-        padding: 0px;
-    }
-`;
-
-
-const Wrapper = styled.div`
-    position: relative;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    flex-direction: column;
-    width: 100%;
-    max-width: 1350px;
-    padding: 80px 0;
-    gap: 12px;
-    @media (max-width: 960px) {
-        flex-direction: column;
-    }
-`;
-
-const Title = styled.div`
-font-size: 42px;
-text-align: center;
-font-weight: 600;
-margin-top: 20px;
-  color: ${({ theme }) => theme.text_primary};
-  @media (max-width: 768px) {
-      margin-top: 12px;
-      font-size: 32px;
-  }
-`;
-
-const Desc = styled.div`
-    font-size: 18px;
-    text-align: center;
-    max-width: 600px;
-    color: ${({ theme }) => theme.text_secondary};
-    @media (max-width: 768px) {
-        margin-top: 12px;
-        font-size: 16px;
-    }
-`;
 
 const Projects = () => {
   return (
@@ -63,9 +12,9 @@ const Projects = () => {
           I have worked on a wide range of projects. From CMS to Web Apps. Here are some of my projects.
         </Desc>
 
-        <ToggleGroup>
+        <ToggleButtonGroup>
           <ToggleButton>Web</ToggleButton>
-        </ToggleGroup>
+        </ToggleButtonGroup>
       </Wrapper>
     </Container>
   );
